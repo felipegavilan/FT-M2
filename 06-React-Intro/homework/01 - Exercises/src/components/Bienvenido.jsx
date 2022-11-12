f5df4caa93148843063af0af77b4703338418f61
felipegavilan <felipegaviilan@gmail.com>
2022-11-12 19:49:37 -0300

    React-Intro

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Felipe";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return(
+  <div>
+    <h1>Bienvenido</h1>
+    <h3>{studentName}</h3>
+    <ul>
+      {
+        techSkills.map((list, index) => (<li key={index}>{list}</li>))
+      }
+      </ul>
+    <Botones alerts={alerts} /> 
+  </div>
+  )
 }
 
 // Esto lo exportamos para los tests
-export { studentName, techSkills, alerts };
+export { studentName, techSkills, alerts, };
--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,5 +2,19 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div></div>;
+   return (
+   <div>
+      {
+         characters.map(m => (
+            <Card
+            name = {m.name}
+            species = {m.species}
+            gender = {m.gender}
+            image = {m.image}
+            onClose = {()=>{alert(m.onClose)}}
+            />
+         ))
+      }
+   </div>
+   );
 }
